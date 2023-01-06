39b1bb1bc21c51cb1c3dc5489a97337b61e76ab7
Michael Murray <mcmurray0125@gmail.com>
2023-01-06 17:48:12 -0600

    started contact component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import Overlay from "./components/Overlay"
 import AboutMe from "./components/AboutMe"
 import Projects from "./components/Projects"
+import Contact from "./components/Contact"
 
 export default function App() {
     return (
@@ -12,6 +13,7 @@ export default function App() {
             <Hero />
             <AboutMe />
             <Projects />
+            <Contact />
         </div>
     )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import '../assets/contact-styles.css'
+
+export default function Contact() {
+    return (
+        <section className="contact">
+            <div className="contact-text">
+                <h1 id="c-title-1" className="contact-title">Got a project in</h1>
+                <h1 id="c-title-2" className="contact-title">mind?</h1>
+            </div>
+            <div className="contact-form">
+                Form goes here
+            </div>
+        </section>
+    )
+}
