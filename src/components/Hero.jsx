973536a3de90e91344e46b361f4c5b9825a92eab
Michael Murray <mcmurray0125@gmail.com>
2023-01-11 00:32:27 -0600

    added media queries for project grid and footer. Adjusted navbar hiding function to only display again when scrollY is 10px from top of screen. Fixes mobile scrolling bug.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
                 <Link to="projects" spy={true} smooth={true} offset={50} duration={1000} className="hero-btn" id="project-btn">Projects &nbsp; <i className="fa-solid fa-screwdriver-wrench"></i></Link>
                 <Link to="contact" spy={true} smooth={true} offset={50} duration={1200} className="hero-btn" id="lets-talk">Let's Talk </Link >
             </div>
-            {<img id='curly-arrow' src={curlyArrow} alt="curly-arrow" />}
+            <img id='curly-arrow' src={curlyArrow} alt="curly-arrow" />
         </div>
         <div className="hero-images">
             <img id="person-one" src={personOne} alt="person-coding" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
   const [visible, setVisible] = useState(true);
 
   const handleScroll = debounce(() => {
-    const currentScrollPos = window.pageYOffset;
+    const currentScrollPos = window.scrollY;
 
-    setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 70) || currentScrollPos < 10);
+    setVisible(currentScrollPos < 10);
 
     setPrevScrollPos(currentScrollPos);
   }, 5);
@@ -27,7 +27,7 @@ const Navbar = () => {
     <div className='nav-container' style={{top: visible ? '0' : '-130px' }} id="navbar">
       <h2 className='nav-brand'>MichaelMurray</h2>
       <div className="nav-links">
-        <Link to="about-me" spy={true} smooth={true} offset={-100} duration={900} >About Me</Link>
+        <Link to="about-me" spy={true} smooth={true} offset={0} duration={900} >About Me</Link>
         <Link to="projects" spy={true} smooth={true} offset={0} duration={1200} >Projects</Link>
         <Link to="contact" spy={true} smooth={true} offset={0} duration={1400} >Contact</Link>
       </div>
