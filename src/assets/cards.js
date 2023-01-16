3b156778174b06b2527ca8fee39b4e4fb8db834f
Michael Murray <mcmurray0125@gmail.com>
2023-01-15 22:57:40 -0600

    added fabchat and tenzies to projects section. Adjusted height properties for projects section so the page grows as the page contents expand.

--- src/assets/cards.js
+++ src/assets/cards.js
@@ -9,20 +9,30 @@ export default [
     },
     {
         id: 2,
-        title: "MM Portfolio Website",
-        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempus neque non finibus bibendum. Mauris consequat pretium varius. Donec non lobortis lacus, quis pulvinar sapien. Aliquam erat volutpat. Curabitur blandit magna orci, vitae rutrum est feugiat vel. Curabitur commodo finibus.",
-        img: '/images/portfolio-img-2.png',
-        link: 'https://github.com/mcmurray0125/mm-portfolio',
+        title: "Tenzies",
+        about: "In Tenzies, you win when all 10 of your dice are the same number! You can click on a die to hold its current value. All dice that are not held will be re-rolled when you click 'Roll Dice'. Let's see how quickly you can get tenzies! Made with React.js",
+        img: '/images/tenzies-img.png',
+        link: "https://mm-tenzies.netlify.app",
+        github: "https://github.com/mcmurray0125/tenzies-game"
     },
     {
         id: 3,
-        title: "Bug Tracker",
-        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu diam eu justo pharetra cursus. Donec mattis turpis urna, vitae consequat metus commodo et. Quisque vulputate scelerisque feugiat. Fusce lectus leo, facilisis sit amet eleifend at, pretium quis odio. Quisque.",
+        title: "Fabchat",
+        about: "Fabchat is an online messaging platform. I am a contributing software developer for Fabchat. My most recent contribution to the platform was to create a new design theme for the web app. Currently, I am utilizing Tailwind CSS to make the platform's dashboard mobile-friendly.",
         img: 'https://images.unsplash.com/photo-1614332287897-cdc485fa562d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-        link: "#",
+        link: "https://fabchat.vercel.app/",
+        github: "https://github.com/harshjohar/fabchat" ,
     },
     {
         id: 4,
+        title: "MM Portfolio Website",
+        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempus neque non finibus bibendum. Mauris consequat pretium varius. Donec non lobortis lacus, quis pulvinar sapien. Aliquam erat volutpat. Curabitur blandit magna orci, vitae rutrum est feugiat vel. Curabitur commodo finibus.",
+        img: '/images/portfolio-img-2.png',
+        link: '#',
+        github: "https://github.com/mcmurray0125/mm-portfolio"
+    },
+    {
+        id: 5,
         title: "Meme Creator",
         about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi laoreet justo nec neque rutrum dapibus. Nulla a laoreet libero, eu egestas est. Donec odio diam, lobortis hendrerit sapien a, ultricies hendrerit dui. Nulla fermentum lacinia nibh vel mollis. Donec justo.",
         img: 'https://images.unsplash.com/photo-1614332287897-cdc485fa562d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
