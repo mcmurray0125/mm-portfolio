4fdc37e48a311f5e9b0aece3fd25ba7ad7b71167
Michael Murray <mcmurray0125@gmail.com>
2023-06-18 18:30:21 -0500

    form disabled w/ empty inputs.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,25 @@
 import '../assets/contact-styles.css'
-import keyboard from '../assets/keyboard.svg'
-import mail from '../assets/mail.svg'
+import { useState } from 'react'
+
 import arrowTwo from '../assets/arrow-2.svg'
 import personThree from '../assets/person-3.svg'
 import shadow from '../assets/shadow-2.svg'
 
 export default function Contact() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+    const handleMessage = (e) => {
+        setMessage(e.target.value)
+    }
+
     return (
         <section id='contact' className="contact">
             <div className="contact-text">
@@ -24,16 +38,47 @@ export default function Contact() {
                 <div className="form-top">
                     <div className="user-name">
                         <label htmlFor="name">Your name</label>
-                        <input type="text" placeholder='Name' name='name' id= "name" className='top-input' />
+                        <input
+                            type="text"
+                            placeholder='Name'
+                            name='name'
+                            value={name}
+                            onChange={handleName}
+                            autoComplete='on'
+                            id= "name"
+                            className='top-input'
+                        />
                     </div>
                     <div className="user-email">
                         <label htmlFor="email">Your email</label>
-                        <input id= "email" type="email" placeholder='Email' name='email' className='top-input'/>
+                        <input
+                            id= "email"
+                            type="email"
+                            placeholder='Email'
+                            name='email'
+                            value={email}
+                            autoComplete='on'
+                            onChange={handleEmail}
+                            className='top-input'
+                        />
                     </div>
                 </div>
                 <label htmlFor="message">Your message</label>
-                <textarea id= 'message' name='message' placeholder='Message'/>
-               <button id='contact-btn' type="submit" value="Send Message">Send Message &nbsp;<i className="fa-regular fa-paper-plane"></i></button>
+                <textarea
+                    id= 'message'
+                    name='message'
+                    value={message}
+                    onChange={handleMessage}
+                    placeholder='Message'
+                />
+               <button
+                    id='contact-btn'
+                    type="submit"
+                    value="Send Message"
+                    disabled={!(name && email && message)}
+                >
+                    Send Message &nbsp;<i className="fa-regular fa-paper-plane"></i>
+                </button>
             </form>
         </section>
     )
