356a563a5694987f91549ab49a1818781566f47b
Michael Murray <mcmurray0125@gmail.com>
2024-12-17 19:37:31 -0600

    clean up Projects section child component mapping

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,22 +4,20 @@ import ProjectCard from './ProjectCard'
 
 export default function Projects() {
 
-    const cardElements = data.map((item) => {
-        return (
-            <ProjectCard
-                key={item.id}
-                item={item}
-                />
-        )
-      })
-
     return(
         <section className="projects" id='projects'>
             <div className="projects-text">
                 <h1 id="p-title-1">My recent</h1>
                 <h1 id="p-title-2">projects</h1>
             </div>
-            {cardElements}
+            {data.map((item) => {
+                return (
+                    <ProjectCard
+                        key={item.id}
+                        item={item}
+                    />
+                )
+            })}
         </section>
     )
 }
